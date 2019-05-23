201b8aa1e5ff0a6c09913d0f658873e59b0b2dc9
Isabela Silveira <isabelatsilveira@gmail.com>
2019-05-23 00:56:47 -0300

    modal de cadastro/login, estilo do card de produtos

--- bootstrap-4.3.1/js/custom-script.js
+++ bootstrap-4.3.1/js/custom-script.js
@@ -1,11 +1,28 @@
-$('.dropdown-submenu > a').on("click", function(e) {
-    var submenu = $(this);
-    $('.dropdown-submenu .dropdown-menu').removeClass('show');
-    submenu.next('.dropdown-menu').addClass('show');
-    e.stopPropagation();
+jQuery('document').ready(function(){
+    jQuery('.dropdown-submenu > a').on("click", function(e) {
+        var submenu = jQuery(this);
+        jQuery('.dropdown-submenu .dropdown-menu').removeClass('show');
+        submenu.next('.dropdown-menu').addClass('show');
+        e.stopPropagation();
+    });
+    
+    jQuery('.dropdown').on("hidden.bs.dropdown", function() {
+        // hide any open menus when parent closes
+        jQuery('.dropdown-menu.show').removeClass('show');
+    });
+    
 });
 
-$('.dropdown').on("hidden.bs.dropdown", function() {
-    // hide any open menus when parent closes
-    $('.dropdown-menu.show').removeClass('show');
+jQuery("window").scroll( function(){
+
+    console.log('teste');
+
+    var header = jQuery(document).scrollTop();
+    var headerHeight = jQuery("#header").outerHeight();
+
+    if(header > headerHeight){
+        jQuery('#header').addClass('sticky-top');
+    }else{
+        jQuery('#header').removeClass('sticky-top');
+    }
 });
