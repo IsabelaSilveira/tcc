dbf56eaa2de1a379d08532233988e662e5cf4001
Isabela Silveira <isabelatsilveira@gmail.com>
2019-06-16 20:09:50 -0300

    Ajustes menu - Foundation

--- foundation-6/js/custom-script.js
+++ foundation-6/js/custom-script.js
@@ -1,78 +1,85 @@
-jQuery('document').ready(function(){
-    jQuery('.dropdown-submenu > a').on("click", function(e) {
-        var submenu = jQuery(this);
-        jQuery('.dropdown-submenu .dropdown-menu').removeClass('show');
-        submenu.next('.dropdown-menu').addClass('show');
-        e.stopPropagation();
-    });
-    
-    jQuery('.dropdown').on("hidden.bs.dropdown", function() {
-        // hide any open menus when parent closes
-        jQuery('.dropdown-menu.show').removeClass('show');
-    });
+$('document').ready(function(){
 
-    if(jQuery("#cart-preview tr").length == 0){
+    if($("#cart-preview tr").length == 0){
         prod_item = '<tr id="cart-empty" class="no-gutters"><td>Nenhum produto adicionado</td></tr>';
-        jQuery("#cart-preview .table tbody").append(prod_item); 
+        $("#cart-preview .table tbody").append(prod_item); 
     }
 
+    var addedProductCodes = [];
+    $('.cart-add').on('click', function(){
+    
+
+        //adicionando ao carrinho
+        var prod_id, prod_qtd, qtd_old, qtd_new, qtd_final , prod_name, prod_thumb, prod_price, prod_item; 
+
+        prod_id = $(this).parents('.card').attr('data-id'); 
+        prod_qtd = $(this).parents('.card').find('.prod-qtd').val(); 
+        prod_name = $(this).parents('.card').find('.card-title').attr('data-name'); 
+        prod_thumb = $(this).parents('.card').find('.card-img-top').attr('data-img'); 
+        prod_price = $(this).parents('.card').find('.card-price').attr('data-price'); 
+        price_new = prod_price.replace(/\,00$/,'');  
+
+        $('#cart-empty').remove();
+        prod_item = 
+        '<tr id="'+prod_id+'" class="no-gutters prod-preview">'+
+            '<td class="column-thumb"><img src="'+prod_thumb+'" alt="Miniatura do produto" class="img-fluid"></td>'+
+            '<td class="text-left column-preview"><a href="#" title="'+prod_name+'" class="preview-link">'+prod_name+'</a></td>'+
+            '<td class="text-center column-qtd">'+prod_qtd+'</td>'+
+            '<td class="text-center column-price" data-price="'+price_new+'">R$ '+prod_price+'</td>'+
+            '<td class="text-center"><a href="" title="Remover produto">X</td>'+
+        '</tr>';
+
+        var index = $.inArray(prod_id, addedProductCodes);
+
+        if (index >= 0) {
+            $('.prod-preview').each(function(){
+                if($(this).attr('id') == prod_id ){
+                    qtd_old = parseInt($(".column-qtd", this).text());
+                    qtd_new = parseInt(prod_qtd); 
+                    qtd_final = parseInt(qtd_old + qtd_new);
+    
+                    $(".column-qtd", this).text(qtd_final);
+                }
+            });
+            
+        }else{
+            $("#cart-preview .table tbody").append(prod_item);   
+            addedProductCodes.push(prod_id);
+        }
+
+        var itens_total = 0, price_unit = 0, price_total = 0;
+        $('.prod-preview').each(function(){
+            itens_total += parseInt($(".column-qtd", this).text()); 
+            price_unit = parseInt($(".column-price", this).attr('data-price')) ; 
+            prod_price = itens_total * price_unit;
+
+            price_total += prod_price;
+            
+        });
+        $('#qtd-itens').text(itens_total);
  
-        jQuery('.cart-add').on('click', function(){
+
+        //push notification
+        $("#modalNotification").addClass('animated slideInUp').show(); 
+        $("#notified-thumb").attr('src', prod_thumb); 
+        $("#notified-link").attr('title', prod_name).text(prod_name); 
         
-
-            //adicionando ao carrinho
-            var prod_id, prod_qtd, qtd_old, qtd_new, qtd_final , prod_name, prod_thumb, prod_price, prod_item; 
-    
-            prod_id = jQuery(this).parents('.card').attr('data-id'); 
-            prod_qtd = jQuery(this).parents('.card').find('.prod-qtd').val(); 
-            prod_name = jQuery(this).parents('.card').find('.card-title').attr('data-name'); 
-            prod_thumb = jQuery(this).parents('.card').find('.card-img-top').attr('data-img'); 
-            prod_price = jQuery(this).parents('.card').find('.card-price').attr('data-price');   
-    
-            jQuery('#cart-empty').remove();
-    
-            if(jQuery('.prod-preview').attr('id') == prod_id ){
-                qtd_old = parseInt(jQuery("#cart-preview .column-qtd").text());
-                qtd_new = parseInt(prod_qtd); 
-                qtd_final = parseInt(qtd_old + qtd_new);
-    
-                jQuery("#cart-preview .column-qtd").text(qtd_final);
-            }else{
-                prod_item = 
-                '<tr id="'+prod_id+'" class="no-gutters prod-preview">'+
-                    '<td class="column-thumb"><img src="'+prod_thumb+'" alt="Miniatura do produto" class="img-fluid"></td>'+
-                    '<td class="text-left column-preview"><a href="#" title="'+prod_name+'" class="preview-link">'+prod_name+'</a></td>'+
-                    '<td class="text-center column-qtd">'+prod_qtd+'</td>'+
-                    '<td class="text-center column-price">R$ '+prod_price+'</td>'+
-                    '<td class="text-center"><a href="" title="Remover produto">X</td>'+
-                '</tr>';
-            }
-
-            console.log(prod_item);
-    
-            jQuery("#cart-preview .table tbody").append(prod_item);    
-    
-            //push notification
-            jQuery("#modalNotification").addClass('animated slideInUp').show(); 
-            jQuery("#notified-thumb").attr('src', prod_thumb); 
-            jQuery("#notified-link").attr('title', prod_name).text(prod_name); 
-            
-            setTimeout(function(){
-                jQuery("#modalNotification").removeClass('animated slideInUp').hide(); 
-            }, 3000);
-            
-        })
-    });
+        setTimeout(function(){
+            $("#modalNotification").removeClass('animated slideInUp').hide(); 
+        }, 6000);
+        
+    })
+});
     
 
-jQuery(window).on('scroll', function(){
+$(window).on('scroll', function(){
 
-    var header = jQuery(document).scrollTop();
-    var headerHeight = jQuery("#header").outerHeight();
+    var header = $(document).scrollTop();
+    var headerHeight = $("#header").outerHeight();
 
     if(header > headerHeight){
-        jQuery('#header').addClass('sticky-top');
+        $('#header').addClass('sticky-top');
     }else{
-        jQuery('#header').removeClass('sticky-top');
+        $('#header').removeClass('sticky-top');
     }
 });
